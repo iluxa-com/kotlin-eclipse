fdbbdfb4750f87cd4773dfc2464122e76b6852fc
unknown <rostov93@gmail.com>
2015-01-30 19:21:44 +0300

    test

---
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/debug/KotlinBreakpointMarkerUpdater.java
@@ -0,0 +1,27 @@
+package org.jetbrains.kotlin.ui.debug;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.Position;
+import org.eclipse.ui.texteditor.IMarkerUpdater;
+
+public class KotlinBreakpointMarkerUpdater implements IMarkerUpdater {
+
+    @Override
+    public String getMarkerType() {
+//        return KotlinLineBreakpoint.ID_MARKER;
+        return null;
+    }
+
+    @Override
+    public String[] getAttribute() {
+        return new String[] { IMarker.LINE_NUMBER };
+    }
+
+    @Override
+    public boolean updateMarker(IMarker marker, IDocument document, Position position) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
