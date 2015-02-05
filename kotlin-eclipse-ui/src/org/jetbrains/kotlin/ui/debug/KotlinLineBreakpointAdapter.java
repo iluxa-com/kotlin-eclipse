0af3c63e6e228e5cc082fe8db1a110712c739890
unknown <mikhail.zarechenskiy@jetbrains.com>
2015-02-05 15:57:56 +0300

    Use JDT breakpoints

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/debug/KotlinLineBreakpointAdapter.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/debug/KotlinLineBreakpointAdapter.java
@@ -1,12 +1,11 @@
 package org.jetbrains.kotlin.ui.debug;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.IBreakpointManager;
-import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.ui.actions.IToggleBreakpointsTarget;
 import org.eclipse.jdt.debug.core.IJavaLineBreakpoint;
 import org.eclipse.jdt.debug.core.JDIDebugModel;
@@ -20,7 +19,6 @@ import org.eclipse.ui.texteditor.ITextEditor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.core.builder.KotlinPsiManager;
-import org.jetbrains.kotlin.core.debug.KotlinLineBreakpoint;
 import org.jetbrains.kotlin.core.log.KotlinLogger;
 import org.jetbrains.kotlin.load.kotlin.PackageClassUtils;
 import org.jetbrains.kotlin.name.FqName;
@@ -40,7 +38,7 @@ public class KotlinLineBreakpointAdapter implements IToggleBreakpointsTarget {
             IDocument document = editor.getDocumentProvider().getDocument(editor.getEditorInput());
             String typeName = getTypeName(document, lineNumber, (IFile) resource);
             
-            IJavaLineBreakpoint existingBreakpoint = findKotlinBreakpoint(resource, lineNumber);
+            IJavaLineBreakpoint existingBreakpoint = JDIDebugModel.lineBreakpointExists(resource, typeName, lineNumber);
             if (existingBreakpoint != null) {
                 existingBreakpoint.delete();
             } else {
@@ -54,8 +52,8 @@ public class KotlinLineBreakpointAdapter implements IToggleBreakpointsTarget {
                     KotlinLogger.logAndThrow(e);
                 }
                 
-                KotlinLineBreakpoint kotlinLineBreakpoint = KotlinLineBreakpoint.createKotlinLineBreakpoint(resource, typeName, lineNumber, charStart, charEnd);
-                DebugPlugin.getDefault().getBreakpointManager().addBreakpoint(kotlinLineBreakpoint);
+                Map<String, Object> attributes = new HashMap<String, Object>(10);
+                JDIDebugModel.createLineBreakpoint(resource, typeName, lineNumber, charStart, charEnd, 0, true, attributes);
             }
         }
     }
@@ -86,28 +84,6 @@ public class KotlinLineBreakpointAdapter implements IToggleBreakpointsTarget {
     public boolean canToggleWatchpoints(IWorkbenchPart part, ISelection selection) {
         // TODO Auto-generated method stub
         return true;
-    }
-    
-    @Nullable
-    private IJavaLineBreakpoint findKotlinBreakpoint(IResource resource, int lineNumber) throws CoreException {
-        String markerType = KotlinLineBreakpoint.KOTLIN_LINE_BREAKPOINT_MARKER;
-        IBreakpointManager manager = DebugPlugin.getDefault().getBreakpointManager();
-        IBreakpoint[] breakpoints = manager.getBreakpoints(JDIDebugModel.getPluginIdentifier());
-        for (IBreakpoint breakpoint : breakpoints) {
-            if (!(breakpoint instanceof KotlinLineBreakpoint)) {
-                continue;
-            }
-            
-            KotlinLineBreakpoint kotlinLineBreakpoint = (KotlinLineBreakpoint) breakpoint;
-            IMarker marker = kotlinLineBreakpoint.getMarker();
-            if (marker != null && marker.exists() && markerType.equals(marker.getType())) {
-                if (kotlinLineBreakpoint.getLineNumber() == lineNumber) {
-                    return kotlinLineBreakpoint;
-                }
-            }
-        }
-        
-        return null;
     }
     
     @NotNull
