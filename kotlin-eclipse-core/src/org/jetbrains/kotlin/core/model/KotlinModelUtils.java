c193a26ef9d6408ea4a01714ea2626a5d87d5f25
unknown <mikhail.zarechenskiy@jetbrains.com>
2015-02-16 12:38:06 +0300

    Exclude Kotlin files from output

---
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/model/KotlinModelUtils.java
@@ -0,0 +1,19 @@
+package org.jetbrains.kotlin.core.model;
+
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.jetbrains.annotations.NotNull;
+
+public class KotlinModelUtils {
+    public static void excludeKotlinFilesFromOutput(@NotNull IJavaProject javaProject) {
+        String excludeFiles = javaProject.getOption(JavaCore.CORE_JAVA_BUILD_RESOURCE_COPY_FILTER, true);
+        if (!excludeFiles.contains("*.kt")) {
+            if (!excludeFiles.isEmpty()) {
+                excludeFiles = excludeFiles.concat(",");
+            }
+            excludeFiles = excludeFiles.concat("*.kt");
+            
+            javaProject.setOption(JavaCore.CORE_JAVA_BUILD_RESOURCE_COPY_FILTER, excludeFiles);
+        }
+    }
+}
